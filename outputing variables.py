370e637c36915f113d58d8475a3b791ad85972f1
Ed ward <52161163+kasule8910@users.noreply.github.com>
2022-10-22 23:14:03 +0300

    Add files via upload

---
+++ outputing variables.py
@@ -0,0 +1,13 @@
+#output variables
+x = 100
+y = " Pals"
+print(x, y)
+
+a = "Python "
+b = " is"
+c = " awsome"
+print(a + b + c)
+
+d = 13
+e = 7
+print(d + e)
---
+++ unpacking collections.py
@@ -0,0 +1,7 @@
+#unpacking a collection (list, tuple etc)
+#extracting values into variables
+fruits = ["Oranges", "apple", "bananas"]
+x,y,z = fruits
+print(x)
+print(y)
+print(z)

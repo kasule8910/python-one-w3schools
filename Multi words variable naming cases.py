c082839a536b492aeefd361a2304d869f8e9a270
Ed ward <52161163+kasule8910@users.noreply.github.com>
2022-10-22 11:38:17 +0300

    Add files via upload

---
+++ Multi words variable naming cases.py
@@ -0,0 +1,12 @@
+#Multi words variable Names
+#1.Camel case
+myVariableName = 10
+print(myVariableName)
+
+#2.Pascal case
+MyVariableName = 20
+print(MyVariableName)
+
+#3.Snake case
+my_variable_name = 30
+print(my_variable_name)
---
+++ Mutiple value assignment.py
@@ -0,0 +1,9 @@
+#multi variable assignments
+x,y,z = "Orange",2,false
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
